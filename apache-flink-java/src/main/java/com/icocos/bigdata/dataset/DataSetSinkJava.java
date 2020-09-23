e4d5ef20326b0baa8b3775ae1354c0ba605f78a4
曹理鹏 <al10201119@163.com>
2020-09-23 22:12:25 +0800

    增加Sink类

---
+++ apache-flink-java/src/main/java/com/icocos/bigdata/dataset/DataSetSinkJava.java
@@ -0,0 +1,9 @@
+package com.icocos.bigdata.dataset;
+
+public class DataSetSinkJava {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ apache-flink-scala/src/main/scala/com/icocos/bigdata/dataset/DataSetSinkScala.scala
@@ -0,0 +1,9 @@
+package com.icocos.bigdata.dataset
+
+object DataSetSinkScala {
+
+  def main(args: Array[String]): Unit = {
+    
+  }
+
+}
