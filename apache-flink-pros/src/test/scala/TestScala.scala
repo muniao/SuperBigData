452c9fa90f0c5932b578ed046af276a91ff17955
曹理鹏 <al10201119@163.com>
2020-09-23 20:12:31 +0800

    优化代码逻辑

--- apache-flink-pros/src/test/scala/TestScala.scala
+++
@@ -1,8 +0,0 @@
-//to enable implicit conversion instead of creating implicit value for each type that you use.
-//需要手动导包
-
-object TestScala {
-  def main(args: Array[String]): Unit = {
-    print("Hello Scala")
-  }
-}
