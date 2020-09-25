07b649e168a98fa3c147b342e594f7067714e2f1
曹理鹏 <al10201119@163.com>
2020-09-25 18:08:56 +0800

    TableApiAndSQL

---
+++ apache-flink-java/src/main/java/com/icocos/bigdata/tablesql/TableApiAndSQLJava.java
@@ -0,0 +1,9 @@
+package com.icocos.bigdata.tablesql;
+
+public class TableApiAndSQLJava {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ apache-flink-scala/src/main/scala/com/icocos/bigdata/tablesql/TableApiAndSQLScala.scala
@@ -0,0 +1,9 @@
+package com.icocos.bigdata.tablesql
+
+class TableApiAndSQLScala {
+
+  def main(args: Array[String]): Unit = {
+    
+  }
+
+}
