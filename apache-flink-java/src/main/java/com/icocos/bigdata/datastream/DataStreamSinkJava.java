2e0e1a8ea518ce8b2b871c59afe06eea0b694959
曹理鹏 <al10201119@163.com>
2020-09-25 18:05:29 +0800

    处理MySQLSink

--- apache-flink-java/src/main/java/com/icocos/bigdata/datastream/DataStreamSinkJava.java
+++ apache-flink-java/src/main/java/com/icocos/bigdata/datastream/DataStreamSinkJava.java
@@ -3,14 +3,15 @@ package com.icocos.bigdata.datastream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class DataStreamSinkJava {
-    public static void main(String[] args) throws Exception {
 
+    public static void main(String[] args) throws Exception {
         // 1. 获取上下文
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         sinkFunction(env);
         env.execute();
     }
 
+    // practical-pros/src/main/java/flink/kafka/mysql
     public static void sinkFunction(StreamExecutionEnvironment env) throws Exception {
 
     }
--- apache-flink-scala/src/main/scala/com/icocos/bigdata/datastream/DataStreamSinkScala.scala
+++ apache-flink-scala/src/main/scala/com/icocos/bigdata/datastream/DataStreamSinkScala.scala
@@ -5,13 +5,13 @@ import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 object DataStreamSinkScala {
 
   def main(args: Array[String]): Unit = {
-
     // 1. 获取上下文
     val env = StreamExecutionEnvironment.getExecutionEnvironment;
     sinkFunction(env)
     env.execute
   }
 
+  // practical-pros/src/main/java/flink/kafka/mysql
   def sinkFunction(env: StreamExecutionEnvironment): Unit = {
 
   }
